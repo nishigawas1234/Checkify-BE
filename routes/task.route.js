e5e370580a3b200e7d05f5f0016757a8262c06fb
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-07-16 20:11:14 +0530

    feat: update task

--- routes/task.route.js
+++ routes/task.route.js
@@ -4,6 +4,7 @@ const taskController = require("../controllers/task.controller");
 
 router.post("/task/addData/:user_id", taskController.addData);
 router.get("/task/getData/:user_id", taskController.getData);
+router.patch("/task/updateTask/:user_id/:task_id", taskController.updateTask);
 
 
 module.exports = router;
